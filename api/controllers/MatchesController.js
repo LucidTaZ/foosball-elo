d66dce6eea8239905d60062e6b535eee308fcc2e
Thijs Zumbrink <tazaranie@gmail.com>
2018-05-27 15:19:01 +0200

    Prevent event projection on validation errors

--- api/controllers/MatchesController.js
+++ api/controllers/MatchesController.js
@@ -6,28 +6,23 @@ var MatchConcludedModel = mongoose.model('MatchConcluded');
 var Players = require('../projections/Players');
 var Matches = require('../projections/Matches');
 
-var respond = function(res) {
-    return function(err, data) {
-        if (err)
-            res.send(err);
-        res.json(data);
-    };
-};
-
 exports.list = function(req, res) {
-    respond(res)(null, Matches.all());
+    res.json(Matches.all());
 };
 
 exports.show = function(req, res) {
-    respond(res)(null, Matches.find(req.params.id));
+    res.json(Matches.find(req.params.id));
 };
 
 exports.create = function(req, res) {
     var newMatchConcluded = new MatchConcludedModel(req.body);
-    newMatchConcluded.save(respond(res));
-
-    // FIXME: Order is dependent since Matches uses the current players'
-    // ratings to show deltas, while Players updates them.
-    Matches.projectMatchConcluded(newMatchConcluded);
-    Players.projectMatchConcluded(newMatchConcluded);
+    newMatchConcluded.save(function(err, data) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            Matches.projectMatchConcluded(newMatchConcluded);
+            Players.projectMatchConcluded(newMatchConcluded);
+        }
+        res.json(data);
+    });
 };
--- api/controllers/PlayersController.js
+++ api/controllers/PlayersController.js
@@ -4,24 +4,33 @@ var mongoose = require('mongoose');
 var PlayerRegisteredModel = mongoose.model('PlayerRegistered');
 var Players = require('../projections/Players');
 
-var respond = function(res) {
+var respondAndThen = function(res, successFunction) {
     return function(err, data) {
-        if (err)
-            res.send(err);
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            successFunction()
+        }
         res.json(data);
     };
 };
 
 exports.list = function(req, res) {
-    respond(res)(null, Players.all());
+    res.json(Players.all());
 };
 
 exports.show = function(req, res) {
-    respond(res)(null, Players.find(req.params.id));
+    res.json(Players.find(req.params.id));
 };
 
 exports.create = function(req, res) {
     var newPlayerRegistered = new PlayerRegisteredModel(req.body);
-    newPlayerRegistered.save(respond(res));
-    Players.projectPlayerRegistered(newPlayerRegistered);
+    newPlayerRegistered.save(function(err, data) {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            Players.projectPlayerRegistered(newPlayerRegistered)
+        }
+        res.json(data);
+    });
 };
